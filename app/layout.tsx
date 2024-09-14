5f16fb2aa62e0c805b00cae102bc5012862a0890
rohitbisht01 <rb9519208@gmail.com>
2024-09-14 16:26:12 +0530

    fix: cv download

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const recursive = Recursive({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Rohit Singh Bisht | Portfolio",
+  title: "Rohit Singh Bisht",
   description:
     "A Full stack developer portfolio with experties in trending technologies",
 };
@@ -17,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><circle cx='50' cy='50' r='50' fill='lightgray'/><text y='55%' x='50%' dominant-baseline='middle' text-anchor='middle' font-size='50' font-family='Arial' fill='black'>R</text></svg>"
+          sizes="any"
+        />
+      </head>
+
       <body className={recursive.className}>
         <main className="container mx-auto max-w-screen-lg sm:max-w-[700px] lg:max-w-xl xl:max-w-3xl py-6 px-4">
           {children}
