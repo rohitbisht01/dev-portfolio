4ebe5174c73f274ee0756ac6108f53fb1cbda408
rohitbisht01 <rb9519208@gmail.com>
2024-09-23 00:50:50 +0530

    feat: added some animation

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,15 +1,18 @@
 import { projectsData } from "@/utils";
 import { ProjectCard } from "./ProjectCard";
+import { TextFade } from "./TextFade";
 
 export default function ProjectsSection() {
   return (
-    <div className="py-2">
-      <h1 className="font-bold text-xl">Projects</h1>
-      <div className="mt-4">
-        {projectsData.map((project) => {
-          return <ProjectCard key={project.link} project={project} />;
-        })}
+    <TextFade direction="up">
+      <div className="py-2">
+        <h1 className="font-bold text-xl">Projects</h1>
+        <div className="mt-4">
+          {projectsData.map((project) => {
+            return <ProjectCard key={project.link} project={project} />;
+          })}
+        </div>
       </div>
-    </div>
+    </TextFade>
   );
 }
--- components/WorkExperienceSection.tsx
+++ components/WorkExperienceSection.tsx
@@ -1,37 +1,42 @@
 import { workExperienceData } from "@/utils";
 import Image from "next/image";
+import { TextFade } from "./TextFade";
 
 const WorkExperienceSection = () => {
   return (
-    <div className="mt-8">
-      <h1 className="font-bold text-xl">Work Experience</h1>
-      <div className="py-4">
-        {workExperienceData.map((experience, index) => {
-          return (
-            <div key={index} className="flex gap-3 pb-4">
-              <Image
-                loading="lazy"
-                src={experience.companyLogo}
-                alt={experience.companyName}
-                width={60}
-                height={60}
-              />
-              <div className="flex flex-col w-full">
-                <div className="flex items-center justify-between w-full">
-                  <h2 className="font-bold text-blue-600 text-lg">
-                    {experience.companyName}
-                  </h2>
-                  <p className="text-sm text-slate-600">{experience.period}</p>
+    <TextFade direction="up">
+      <div className="mt-8">
+        <h1 className="font-bold text-xl">Work Experience</h1>
+        <div className="py-4">
+          {workExperienceData.map((experience, index) => {
+            return (
+              <div key={index} className="flex gap-3 pb-4">
+                <Image
+                  loading="lazy"
+                  src={experience.companyLogo}
+                  alt={experience.companyName}
+                  width={60}
+                  height={60}
+                />
+                <div className="flex flex-col w-full">
+                  <div className="flex items-center justify-between w-full">
+                    <h2 className="font-bold text-blue-600 text-lg">
+                      {experience.companyName}
+                    </h2>
+                    <p className="text-sm text-slate-600">
+                      {experience.period}
+                    </p>
+                  </div>
+                  <p className="italic text-sm text-slate-600">
+                    {experience.position}
+                  </p>
                 </div>
-                <p className="italic text-sm text-slate-600">
-                  {experience.position}
-                </p>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </TextFade>
   );
 };
 
