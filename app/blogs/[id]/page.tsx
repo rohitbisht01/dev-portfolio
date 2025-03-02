c7d6ca5842e0c9097d39872109f6a6eb6ad2eab6
rohitbisht01 <rb9519208@gmail.com>
2025-03-02 09:59:05 +0530

    ts error

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -2,7 +2,7 @@ import path from "path";
 import React from "react";
 import fs from "fs";
 import blogs from "../_content/blogs.json";
-import ReactMarkdown from "react-markdown";
+// import ReactMarkdown from "react-markdown";
 import Link from "next/link";
 
 const BlogDetailsPage = ({ params }: { params: { id: string } }) => {
@@ -11,6 +11,7 @@ const BlogDetailsPage = ({ params }: { params: { id: string } }) => {
   let fileContent;
   try {
     fileContent = fs.readFileSync(filePath, "utf8");
+    console.log(fileContent);
   } catch (error) {
     console.log(error);
   }
@@ -29,65 +30,65 @@ const BlogDetailsPage = ({ params }: { params: { id: string } }) => {
         <h1 className="font-bold text-blue-600 text-2xl">{blog[0].title}</h1>
         <p className="text-gray-500 text-base">{blog[0].created_at}</p>
       </div>
-      <ReactMarkdown
-      // className={` markdown-container w-full mt-10`}
-      // remarkPlugins={[remarkGfm]}
-      // rehypePlugins={[
-      //   rehypeRaw,
-      //   [
-      //     rehypeExternalLinks,
-      //     { target: "_blank", rel: "noopener noreferrer" },
-      //   ],
-      // ]}
-      // components={{
-      //   img: ({ ...props }) => (
-      //     <Zoom>
-      //       <img {...props} style={{ maxWidth: "100%" }} />
-      //     </Zoom>
-      //   ),
-      //   video: ({ ...props }) => {
-      //     const { width = "100%", height = "auto", ...rest } = props;
-      //     return (
-      //       <video
-      //         {...rest}
-      //         controls
-      //         style={{
-      //           width,
-      //           height,
-      //           borderRadius: "8px",
-      //         }}
-      //       >
-      //         Your browser does not support the video tag.
-      //       </video>
-      //     );
-      //   },
-      //   iframe: ({ ...props }) => (
-      //     <span
-      //       style={{
-      //         display: "inline-block",
-      //         position: "relative",
-      //         width: props.width || "640px",
-      //         height: props.height || "360px",
-      //         maxWidth: "100%",
-      //         margin: "0 auto",
-      //       }}
-      //     >
-      //       <iframe
-      //         {...props}
-      //         style={{
-      //           width: "100%",
-      //           height: "100%",
-      //           border: "0",
-      //         }}
-      //         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      //         allowFullScreen
-      //       />
-      //     </span>
-      //   ),
-      // }}
+      {/* <ReactMarkdown
+        className={` markdown-container w-full mt-10`}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[
+          rehypeRaw,
+          [
+            rehypeExternalLinks,
+            { target: "_blank", rel: "noopener noreferrer" },
+          ],
+        ]}
+        components={{
+          img: ({ ...props }) => (
+            <Zoom>
+              <img {...props} style={{ maxWidth: "100%" }} />
+            </Zoom>
+          ),
+          video: ({ ...props }) => {
+            const { width = "100%", height = "auto", ...rest } = props;
+            return (
+              <video
+                {...rest}
+                controls
+                style={{
+                  width,
+                  height,
+                  borderRadius: "8px",
+                }}
+              >
+                Your browser does not support the video tag.
+              </video>
+            );
+          },
+          iframe: ({ ...props }) => (
+            <span
+              style={{
+                display: "inline-block",
+                position: "relative",
+                width: props.width || "640px",
+                height: props.height || "360px",
+                maxWidth: "100%",
+                margin: "0 auto",
+              }}
+            >
+              <iframe
+                {...props}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  border: "0",
+                }}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </span>
+          ),
+        }}
       >
         {fileContent}
-      </ReactMarkdown>
+      </ReactMarkdown> */}
     </section>
   );
 };
